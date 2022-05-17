53260707b01cc467ba29fb723e39dfacb7712f4e
ConanSpinney <cspinney1999@gmail.com>
2022-05-17 06:58:40 +0100

    third comit

--- Projects/nextfire/nextfire/pages/_app.js
+++ Projects/nextfire/nextfire/pages/_app.js
@@ -1,13 +1,20 @@
 import '../styles/globals.css'
 import Navbar from '../components/Navbar';
+import { UserContext } from '../lib/context'
+import { useUserData } from '../lib/hooks' 
 
 
+ 
 function MyApp({ Component, pageProps }) {
+  
+  const userData = useUserData();
+
   return (
-    <>
+    <UserContext.Provider value={{ user: {}, username: 'jeff' }}>
       <Navbar />
       <Component {...pageProps} />
-    </>
+    </UserContext.Provider>
+
   );
 }
 
